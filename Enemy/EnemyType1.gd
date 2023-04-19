cbff0a67426c1164936335eac88547d28e1e516d
Nghia Huynh <114641637+JackHuynh0610@users.noreply.github.com>
2023-04-18 23:47:10 -0400

    Making enemy movement
    This part is enemy movement so it chase the player

--- Enemy/EnemyType1.gd
+++ Enemy/EnemyType1.gd
@@ -1,2 +1,21 @@
 extends KinematicBody2D
 
+export var speed = 350
+var motion = Vector2.ZERO
+
+var player = null 
+
+#Process the game 
+func _physics_process(delta):
+	motion = Vector2.ZERO
+	if player:
+		motion = position.direction_to(player.position) * speed
+	motion = move_and_slide(motion)
+
+#Check if player collide or not
+func _on_Area2D_body_entered(body):
+	player = body 
+
+
+func _on_Area2D_body_exited(body):
+	player = null #set player null so it stop chasing it's target
