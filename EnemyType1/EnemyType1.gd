447d7d47378bda8be4dd19b382d092e6c68be33e
cleithem4 <c4leithem@gmail.com>
2023-04-20 15:34:36 -0400

    fixed

--- EnemyType1/EnemyType1.gd
+++ EnemyType1/EnemyType1.gd
@@ -1,13 +1,10 @@
 extends KinematicBody2D
 
 export var speed = 350
-export var health = 5
-export var damage = 1
+export var health = 10
 
 var motion = Vector2.ZERO
-
-
-
+var attackDmg = 5
 var player = null 
 
 #Process the game 
@@ -16,23 +13,26 @@ func _physics_process(_delta):
 	if player:
 		motion = position.direction_to(player.position) * speed 
 		look_at(player.position)
-		$AnimatedSprite.play("Walk")
-		motion = move_and_slide(motion)
+	motion = move_and_slide(motion)
 
 #Check if player collide or not
 func _on_Area2D_body_entered(body):
+	$Area2D/Moan.play()
+	$AnimatedSprite.play("Walk")
 	player = body
 
-#Area of zombie sight
 func _on_Area2D_body_exited(body):
 	player = null #set player null so it stop chasing it's target
-	
-func damage(damage):
-	health = health - damage
-	if health < 1:
+	$AnimatedSprite.play("Idle")
+
+#Zombie dies if health <= 0
+func damage(dmg):
+	health -= dmg
+	if health <= 0:
+		
 		queue_free()
 
-#Area of zombie attack
-func _on_Area2D2_body_entered(body):
-	if body.name == "Player":
-		body.damage(damage)
+func _on_AttackRange_body_entered(body):
+	if body.has_method("damage"):
+		$CollisionShape2D/Bite.play()
+		body.damage(attackDmg)
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,38 +1,47 @@
 extends KinematicBody2D
 
 export var speed := 400
-export var health = 5
-export var attackDmg = 1
+export var health = 30
 
 var velocity = Vector2()
+
 onready var Bullet = load("res://Bullet/Bullet.tscn")
+
 onready var end_of_gun = $end_of_gun
 #Get user input for player movement
 func get_input():
 	rotation = get_global_mouse_position().angle_to_point(position)
 	var input_direction = Input.get_vector("left", "right", "up", "down")
-	 
+
 	if input_direction:
 		$AnimatedSprite.play("Walk")
 	else:
 		$AnimatedSprite.play("Idle")
 	velocity = input_direction * speed
+	
+	if Input.is_action_just_pressed("shoot"):
+		$AudioStreamPlayer.play()
+	
 
+func shoot():
+	if Input.is_action_just_pressed("shoot"):
+		var bullet = Bullet.instance()
+		var target = get_global_mouse_position()
+		bullet.rotation = get_global_mouse_position().angle_to_point(position)
+		bullet.rotation = get_global_mouse_position().angle_to_point(position)
+		bullet.global_position = end_of_gun.global_position
+		bullet.rotation = rotation + 300
+		get_parent().add_child(bullet)
+		
 
 #Process the game
 func _physics_process(_delta):
 	get_input()
 	move_and_slide(velocity)
-	#Shoots bullet when space is pressed
-	if Input.is_action_just_pressed("shoot"):
-		#$AudioStreamPlayer.play()
-		var bullet = Bullet.instance()
-		var target = get_global_mouse_position()
-		bullet.global_position = end_of_gun.global_position
-		bullet.rotation = rotation + 300
-		get_parent().add_child(bullet)
+	shoot() #Get shoot function
 
+#Player die if health <= 0
 func damage(damage):
-	health = health - damage
-	if health < 1:
-		get_tree().change_scene("res://UI/GameOver.tscn")
+	health -= damage
+	if health <= 0:
+		queue_free()
