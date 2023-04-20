62a38509f5c186cd45182d9b294f5baebdba2935
Nghia Huynh <huynhtrongnghia6102004@gmail.com>
2023-04-20 17:51:46 -0400

    asdas

--- EnemyType1/EnemyType1.gd
+++ EnemyType1/EnemyType1.gd
@@ -10,6 +10,10 @@ var player = null
 #Process the game 
 func _physics_process(_delta):
 	motion = Vector2.ZERO
+	player == null
+	for p in $Area2D.get_overlapping_bodies():
+		if p.is_in_group("Player"):
+			player = p
 	if player:
 		motion = position.direction_to(player.position) * speed 
 		look_at(player.position)
@@ -21,13 +25,6 @@ func _on_Area2D_body_entered(body):
 		player = body
 		$Area2D/Moan.play()
 		$AnimatedSprite.play("Walk")
-	else:
-		move_and_slide(motion)
-	
-
-func _on_Area2D_body_exited(body):
-	player = null #set player null so it stop chasing it's target
-	$AnimatedSprite.play("Idle")
 
 #Zombie dies if health <= 0
 func damage(dmg):
