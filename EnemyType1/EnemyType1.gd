fb0bc0cebb25e81c9ed159945ea9b3bd51f50194
cleithem4 <c4leithem@gmail.com>
2023-04-21 15:21:19 -0400

    added blood particles to zombie

--- EnemyType1/EnemyType1.gd
+++ EnemyType1/EnemyType1.gd
@@ -5,7 +5,9 @@ export var health = 10
 
 var motion = Vector2.ZERO
 var attackDmg = 10
-var player = null 
+var player = null
+
+onready var Blood = load("res://Particles/Blood.tscn") 
 
 #Process the game 
 func _physics_process(_delta):
@@ -29,6 +31,13 @@ func _on_Area2D_body_entered(body):
 #Zombie dies if health <= 0
 func damage(dmg):
 	health -= dmg
+	
+	#adds blood to zombie each time they get shot
+	var blood = Blood.instance()
+	get_tree().current_scene.add_child(blood)
+	blood.global_position = global_position
+	
+	
 	if health <= 0:
 		Global.score += 50
 		queue_free()
